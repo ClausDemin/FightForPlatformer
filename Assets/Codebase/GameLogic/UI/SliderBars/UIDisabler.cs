28e26f934077b807ae5b5208f7b11062d4179f78
ClausDemin <nik50596@gmail.com>
2025-09-28 10:38:16 +0300

    Добавлено отключение UI при смерти

---
+++ Assets/Codebase/GameLogic/UI/SliderBars/UIDisabler.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Codebase.GameLogic.Common.HealthBehavior;
+using UnityEngine;
+
+namespace Assets.Codebase.GameLogic.UI.SliderBars
+{
+    public class UIDisabler: MonoBehaviour
+    {
+        [SerializeField] private SmoothedResourceBar[] _bars;
+        [SerializeField] private HealthComponent _health;
+
+        private void Start()
+        {
+            _health.Death += OnDeath;
+        }
+
+        private void OnDeath() 
+        {
+            if (_bars != null && _bars.Length > 0) 
+            {
+                foreach (var bar in _bars) 
+                { 
+                    bar.gameObject.SetActive(false);
+                }
+            }
+        }
+    }
+}
